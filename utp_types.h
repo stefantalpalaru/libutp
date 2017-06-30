bc83b8043bdbee217673fc2a7222e8971d77e3cf
Mike Gelfand <mikedld@mikedld.com>
2017-06-30 15:26:08 +0300

    Various Windows-related fixes

--- utp_types.h
+++ utp_types.h
@@ -43,7 +43,9 @@
 // hash.cpp needs socket definitions, which is why this networking specific
 // code is inclued in utypes.h
 #ifdef WIN32
+	#undef _CRT_SECURE_NO_DEPRECATE
 	#define _CRT_SECURE_NO_DEPRECATE
+	#undef WIN32_LEAN_AND_MEAN
 	#define WIN32_LEAN_AND_MEAN
 	#include <windows.h>
 	#include <winsock2.h>
